02d4cd418704428b16eedd6f3ec5867e6ce1008a
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2015-01-03 20:09:30 +0100

    Added Matrix Zwickl demo and Matrix blank

---
+++ apps/matrix-blank.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <matrix-client-config.h>
+
+#include "app-common.h"
+
+int main(int argc, char **argv)
+{
+	int retval = 0;
+	if (app_init(argc, argv)) {
+		retval = -1;
+		goto out;
+	}
+
+	if (matrix_cmd(MATRIX_MODE_GRAYSCALE)) {
+		retval = -1;
+		goto out;
+	}
+
+	picture_t *pic = picture_alloc();
+
+	matrix_update(pic);
+
+	picture_free(pic);
+
+ out:
+	app_close();
+
+	return retval;
+}
