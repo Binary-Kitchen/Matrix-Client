78d8a58568765dea4c9edd357e279dd62685d400
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-11-04 22:11:55 +0100

    fullblink: cosmetic fixup
    also fix indentation
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- apps/matrix-fullblink.c
+++ apps/matrix-fullblink.c
@@ -21,24 +21,21 @@ int main(int argc, char **argv)
 	picture_t *pic = picture_alloc();
 
 	int i;
-
 	int timeval;
-
 	const unsigned int start = 100000;
-	
-	for(i = 0 ; i < 176 ; i++)
-	{
-	  if(i<60)
-            timeval = (double)start - (double)(start/1000)*pow(i,1.6);
-	  else
-            timeval = (double)start - (double)(start/1000)*pow(60,1.6) - (double)(start/1000)*pow(i-60,1.2);
 
-    	  picture_full(pic);
-  	  matrix_update(pic);
-  	  usleep(timeval);
-  	  picture_clear(pic);
-  	  matrix_update(pic);
-          usleep(timeval);
+	for(i = 0; i < 176; i++) {
+		if(i<60)
+			timeval = (double)start - (double)(start/1000)*pow(i,1.6);
+		else
+			timeval = (double)start - (double)(start/1000)*pow(60,1.6) - (double)(start/1000)*pow(i-60,1.2);
+
+		picture_full(pic);
+		matrix_update(pic);
+		usleep(timeval);
+		picture_clear(pic);
+		matrix_update(pic);
+		usleep(timeval);
 	}
 
 	picture_free(pic);
